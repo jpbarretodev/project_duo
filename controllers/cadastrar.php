97466b2cc0b49a395a10366db5d36dbcc623aaec
João Paulo <jbarretodasilva7@gmail.com>
2025-07-17 22:38:34 -0400

    atualização geral do projeto

---
+++ controllers/cadastrar.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once "data_base/data_base_connections.php";
+
+// array
+$dts = [$_POST["nome"], $_POST["dataNascimento"], $_POST["mae"], $_POST["pai"], $_POST["responsavel"], $_POST["telefone"], $_POST["deficiencia"], $_POST["endereco"], $_POST["escola"], $_POST["turno"], $_POST["turma"], $_POST["batismo"], $_POST["aut_img"], $_POST["aut_img"], $_POST["resp_igrj"], $_POST["resp_diz"], $_POST["resp_moram_juntos"], $_POST["resp_casados_igrj"]];
+
+
+
+?>
