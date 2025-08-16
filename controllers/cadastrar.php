f0610616055c07cf9eb74d50663220c8de74e4fd
João Paulo <jbarretodasilva7@gmail.com>
2025-08-16 17:26:48 -0400

    atualização do codigo de cadastro

--- controllers/cadastrar.php
+++ controllers/cadastrar.php
@@ -1,9 +1,14 @@
 <?php
 
-include_once "data_base/data_base_connections.php";
+include_once "../connection_data_base/connection.php";
+include_once "../utils/utils.php";
 
 // array
 $dts = [$_POST["nome"], $_POST["dataNascimento"], $_POST["mae"], $_POST["pai"], $_POST["responsavel"], $_POST["telefone"], $_POST["deficiencia"], $_POST["endereco"], $_POST["escola"], $_POST["turno"], $_POST["turma"], $_POST["batismo"], $_POST["aut_img"], $_POST["aut_img"], $_POST["resp_igrj"], $_POST["resp_diz"], $_POST["resp_moram_juntos"], $_POST["resp_casados_igrj"]];
+
+
+// teste de insert
+insert($db, $dts);
 
 
 
